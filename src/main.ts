cca1864d545ee7f09cfac12f5242b2c268a9b562
Aaron Schlegel <aaron@aaronschlegel.me>
2019-08-30 06:35:39 -0700

    add remaining to main routine, update gitignore

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import axios, { AxiosError, AxiosInstance, AxiosResponse } from "axios";
+import { AxiosError, AxiosInstance, AxiosResponse } from "axios";
 
 
 interface PoetryDBClientConfig {
     baseUrl?: string;
 }
+
 
 export class PoetryDBClient {
     public http: AxiosInstance;
@@ -19,24 +20,45 @@ export class PoetryDBClient {
             return response;
         }, (error: AxiosError) => {
             if (error.response) {
-                return Promise.reject(error.request, error.response);
+                return Promise.reject(error.request);
             }
 
             return Promise.reject(error);
         });
     }
 
-    get poetry(): Poetry {
-        return new Poetry(this);
-    }
-}
 
-
-export class Poetry extends PoetryDBClient {
     public async get_poetry(input_term: string, search_term: string, search_type: string,
-                            output: string): Promise<AxiosResponse> {
-        params = {
+                            output: string, output_format: string): Promise<AxiosResponse> {
 
+        if (search_term !== null && input_term !== 'linecount') {
+            search_term = search_term + ':abs'
         }
+
+        if (output_format !== null || output_format !== '') {
+
+            if (output_format !== null) {
+                output = output + '.' + output_format
+            }
+        }
+
+        let parameters = [input_term, search_term, output];
+
+        for (var i = 0; i < parameters.length, i++;) {
+            if (parameters[i] === null ||
+                parameters[i] === '') {
+                parameters.splice(i, 1);
+                i--;
+            }
+        }
+
+        var uri = new URL('/');
+
+        for (p in parameters) {
+            uri = new URL(p, uri);
+        }
+
+        return this.http.get(uri);
     }
+
 }
