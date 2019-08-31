580f3f58c609a5d93cf2c6328492da9ec9282a7a
Aaron Schlegel <aaron@aaronschlegel.me>
2019-08-31 06:07:53 -0700

    commit updates to api wrapper

--- src/main.ts
+++ src/main.ts
@@ -1,64 +1,36 @@
-import { AxiosError, AxiosInstance, AxiosResponse } from "axios";
+import axios from 'axios';
+import { URL } from 'url';
 
 
-interface PoetryDBClientConfig {
-    baseUrl?: string;
-}
+export function get_poetry(input_term: string, search_term: string, search_type: string,
+                        output: string, output_format: string) {
 
-
-export class PoetryDBClient {
-    public http: AxiosInstance;
-    private config: PoetryDBClientConfig;
-
-    constructor(config: PoetryDBClientConfig) {
-        this.config = config;
-        this.http = axios.create({
-            baseURL: config.baseUrl || 'http://poetrydb.org'
-        });
-
-        this.http.interceptors.response.use((response: AxiosResponse) => {
-            return response;
-        }, (error: AxiosError) => {
-            if (error.response) {
-                return Promise.reject(error.request);
-            }
-
-            return Promise.reject(error);
-        });
+    if (search_term !== null && input_term !== 'linecount') {
+        search_term = search_term + ':abs'
     }
 
+    if (output_format !== null || output_format !== '') {
 
-    public async get_poetry(input_term: string, search_term: string, search_type: string,
-                            output: string, output_format: string): Promise<AxiosResponse> {
-
-        if (search_term !== null && input_term !== 'linecount') {
-            search_term = search_term + ':abs'
+        if (output_format !== null) {
+            output = output + '.' + output_format
         }
-
-        if (output_format !== null || output_format !== '') {
-
-            if (output_format !== null) {
-                output = output + '.' + output_format
-            }
-        }
-
-        let parameters = [input_term, search_term, output];
-
-        for (var i = 0; i < parameters.length, i++;) {
-            if (parameters[i] === null ||
-                parameters[i] === '') {
-                parameters.splice(i, 1);
-                i--;
-            }
-        }
-
-        var uri = new URL('/');
-
-        for (p in parameters) {
-            uri = new URL(p, uri);
-        }
-
-        return this.http.get(uri);
     }
 
+    let parameters = [input_term, search_term, output];
+
+    for (var i = 0; i < parameters.length, i++;) {
+        if (parameters[i] === null ||
+            parameters[i] === '') {
+            parameters.splice(i, 1);
+            i--;
+        }
+    }
+
+    let uri = new URL('http://poetrydb.org/');
+
+    for (let p in parameters) {
+        uri = new URL(p, uri);
+    }
+
+    return (axios.get(uri.toString()));
 }
---
+++ tests/main.test.ts
@@ -0,0 +1,7 @@
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import { get_poetry } from "../src/main";
+
+it("Gets poem author", () => {
+
+});
